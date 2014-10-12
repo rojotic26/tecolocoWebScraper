7033a19aefe8cfbebaa74aca1acde0d11525c1a4
mjboyarov <mjboyarov@gmail.com>
2014-10-12 16:48:16 +0800

    added URL variable and method

--- JobSearch.rb
+++ JobSearch.rb
@@ -2,16 +2,27 @@ require 'nokogiri'
 require 'open-uri'
 module JobSearch
 class Tecoloco
-TITLE_JOB = "//div[@class = 'plaza']//a[@class = 'oferta-trabajo']"
-CITY_JOB = "//div[@class = 'plaza']//span[@class = 'ciudad-trabajo']"
-DATE_JOB = "//div[@class = 'plaza']//div[@class = 'vigencia-trabajo']"
-DETAIL_JOB = "//div[@class = 'plaza']//span[@class = 'detalle-trabajo']"
-def self.getJobs(category)
-  doc = get_html(category)
-  titles = get_titles(doc)
-  dates = get_dates(doc)
-  cities = get_cities(doc)
-  details = get_details(doc)
-  integrate (titles,dates, cities, details)
-end
+  TITLE_JOB = "//div[@class = 'plaza']//a[@class = 'oferta-trabajo']"
+  CITY_JOB = "//div[@class = 'plaza']//span[@class = 'ciudad-trabajo']"
+  DATE_JOB = "//div[@class = 'plaza']//div[@class = 'vigencia-trabajo']"
+  DETAIL_JOB = "//div[@class = 'plaza']//span[@class = 'detalle-trabajo']"
+
+  URL = 'http://www.tecoloco.com'
+
+  def self.getJobs(category)
+   doc = get_html(category)
+   titles = get_titles(doc)
+   dates = get_dates(doc)
+   cities = get_cities(doc)
+   details = get_details(doc)
+   integrate (titles,dates, cities, details)
+  end
+
+  def self.getHtml(category)
+   url = "#{URL}/#{category}"
+  end
+
+
+
+
 
